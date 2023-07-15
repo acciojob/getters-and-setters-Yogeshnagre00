7f372a9e2f732c4c52fe18be1c8bdaf19e34477a
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-07-15 21:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
 //complete this code
-class Person {}
+class Person {
+	name;
+	age;
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	set age(age){
+		this.age=age;
+	}
+	
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;//complete this code
+class Person {
+	name;
+	age;
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+
+	get name(){
+		return this.name;
+	}
+
+	set age(age){
+		this.age=age;
+	}
+	
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
